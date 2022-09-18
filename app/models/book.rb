e55a493b0cb3f5f56b6e4718de9d0d1bb291482b
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-09-18 12:21:31 +0000

    閲覧機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,13 @@
 class Book < ApplicationRecord
+  #フォロー機能
   has_many :favorites, dependent: :destroy
+
+  #コメント機能
   has_many :book_comments, dependent: :destroy
+
+  #閲覧数
+  has_many :view_counts, dependent: :destroy
+
   belongs_to :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
@@ -22,7 +29,7 @@ class Book < ApplicationRecord
   end
 
 
-  
+
 def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
   has_many :chats, dependent: :destroy
   has_many :user_rooms, dependent: :destroy
 
+  #閲覧数
+  has_many :view_counts, dependent: :destroy
+
   # フォローをした、されたの関係
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
